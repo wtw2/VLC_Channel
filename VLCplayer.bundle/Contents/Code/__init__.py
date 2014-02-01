39ae5a149ac201aa480b7c80b7f40796df9a8680
wtw2 <walktheway@gmail.com>
2014-02-01 15:54:06 -0600

    Fixed Regex errors
    Corrected and restructured regular expressions.

--- VLCplayer.bundle/Contents/Code/__init__.py
+++ VLCplayer.bundle/Contents/Code/__init__.py
@@ -1,6 +1,7 @@
 # import for regular expressions
 import re
 import datetime
+#import demjson
 
 # http://dev.plexapp.com/docs/api/constkit.html
 
@@ -21,11 +22,20 @@ VLCURL       = 'http://127.0.0.1:11223'
 #VLCURL       = 'http://cs514220v4.vk.me/u5723140/videos/88479e1a6c.360.mp4'
 URL_VLC      = 'http://%s:%s' % (Prefs['vlc_host'], Prefs['vlc_port']) # filled once on start (static)
 
-RE_IP_MAP    = Regex('^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$')
-RE_PORT_MAP  = Regex('^[1-9][0-9]{0,4}$')
-RE_PAGE_MAP  = Regex('^(/[0-9a-zA-Z_\-]*)*(?(1)/)(?:[0-9a-zA-Z_\-\.]+\.[0-9a-zA-Z]{2,4})?$')
-RE_URL_MAP   = Regex('^http://(?:[0-9]{1,3}\.){3}[0-9]{1,3}:[1-9][0-9]{0,4}(?:/[0-9a-zA-Z_\-]*)*(?:/[0-9a-zA-Z_\-\.]+\.[0-9a-zA-Z]{2,4})?$')
-# http://regex101.com/
+ST_IP_MAP    = '(?:[0-9]{1,3}\.){3}[0-9]{1,3}' #WARNING: group must be non-extracting due to use with ST_PATH_MAP in ST_URL_MAP
+RE_IP_MAP    = Regex('^%s$' % (ST_IP_MAP))
+ST_PORT_MAP  = '[1-9][0-9]{0,4}'
+RE_PORT_MAP  = Regex('^%s$' % (ST_PORT_MAP))
+ST_PATH_MAP  = '((/)(?(2)(?:[0-9a-zA-Z_-]+/)+))?'
+ST_FILE_MAP  = '([0-9a-zA-Z_\-\.]+\.[0-9a-zA-Z]{2,4})?'
+ST_PAGE_MAP  = '%s(?(2)|/?)%s' % (ST_PATH_MAP, ST_FILE_MAP) # WARNING: allows for filename only (no slashes)
+RE_PAGE_MAP  = Regex('^%s$' % (ST_PAGE_MAP)) # path is group(1), file is group(3)
+#ST_PAGE_MAP  = '((/)(?(2)(?:[0-9a-zA-Z_-]+/)+))?(?(2)|/?)([0-9a-zA-Z_\-\.]+\.[0-9a-zA-Z]{2,4})?') # path is group(1), file is group(3)
+ST_URL_MAP   = 'http://%s:%s%s' % (ST_IP_MAP, ST_PORT_MAP, ST_PAGE_MAP)
+RE_URL_MAP   = Regex('^%s$' % (ST_URL_MAP))
+#ST_URL_MAP   = '^http://(?:[0-9]{1,3}\.){3}[0-9]{1,3}:[1-9][0-9]{0,4}((/)(?(2)(?:[0-9a-zA-Z_-]+/)+))?(?(2)|/?)([0-9a-zA-Z_\-\.]+\.[0-9a-zA-Z]{2,4})?$'
+
+#ST_URL_MAP = Regex('^(/[0-9a-zA-Z_\-]*)*(?(1)/)(?:[0-9a-zA-Z_\-\.]+\.[0-9a-zA-Z]{2,4})?((/)(?(2)(?:[0-9a-zA-Z_-]+/)+))?(?(2)|/?)([0-9a-zA-Z_\-\.]+\.[0-9a-zA-Z]{2,4})?$')
 
 METADATA     = '{"apiVersion":"2.1","data":{"id":"Hx9TwM4Pmhc","uploaded":"2013-04-25T14:00:46.000Z","updated":"2014-01-27T02:24:39.000Z","uploader":"Unknown","category":"Various","title":"VLC Video Stream","description":"This video is being streamed by VLC player from a direct video URL.","thumbnail":{"sqDefault":"http://i1.ytimg.com/vi/Hx9TwM4Pmhc/default.jpg","hqDefault":"http://i1.ytimg.com/vi/Hx9TwM4Pmhc/hqdefault.jpg"},"player":{"default":"http://www.youtube.com/watch?v=Hx9TwM4Pmhc&feature=youtube_gdata_player","mobile":"http://m.youtube.com/details?v=Hx9TwM4Pmhc"},"content":{"5":"http://www.youtube.com/v/Hx9TwM4Pmhc?version=3&f=videos&app=youtube_gdata","1":"rtsp://r6---sn-o097zuek.c.youtube.com/CiILENy73wIaGQkXmg_OwFMfHxMYDSANFEgGUgZ2aWRlb3MM/0/0/0/video.3gp","6":"rtsp://r6---sn-o097zuek.c.youtube.com/CiILENy73wIaGQkXmg_OwFMfHxMYESARFEgGUgZ2aWRlb3MM/0/0/0/video.3gp"},"duration":3600,"aspectRatio":"widescreen","rating":4.1,"likeCount":"1","ratingCount":1,"viewCount":1,"favoriteCount":1,"commentCount":0,"accessControl":{"comment":"allowed","commentVote":"allowed","videoRespond":"moderated","rate":"allowed","embed":"allowed","list":"allowed","autoPlay":"allowed","syndicate":"allowed"}}}'
 
@@ -63,12 +73,15 @@ def ValidatePrefs():
 	if match == None:
 		return ObjectContainer(header="Error", message="That is not a valid IP port.")
 	Log.Debug("PORT  vlc_port= "+match.group(0))
-	match = re.search(RE_PAGE_MAP, Prefs['vlc_page'])
+	str_page = Prefs['vlc_page']
+	if str_page[0] != '/':
+		str_page = '/' + Prefs['vlc_page'] # does not start with a "/"
+	match = re.search(RE_PAGE_MAP, str_page)
 	if match == None:
 		return ObjectContainer(header="Error", message="That is not a valid page.")
 	Log.Debug("PAGE  vlc_page= "+match.group(0))
 
-	url_vlc = 'http://%s:%s/%s' % (Prefs['vlc_host'], Prefs['vlc_port'], Prefs['vlc_page']) # dynamic
+	url_vlc = 'http://%s:%s%s' % (Prefs['vlc_host'], Prefs['vlc_port'], str_page) # dynamic
 	match = re.search(RE_URL_MAP, url_vlc)
 	if match == None:
 		return ObjectContainer(header="Error", message="That is does not result in a valid url.")
@@ -85,16 +98,25 @@ def MainMenu():
 	oc = ObjectContainer()
 	do = DirectoryObject(key = Callback(SecondMenu), title = "Example Directory")
 	oc.add(do)
+#	eo = createEpisodeObject(
+#		url=VLCURL,
+#		title=TITLE,
+#		summary=TITLE,
+#		thumb=R(ICON),
+#		rating_key=TITLE)
+#	oc.add(eo)
 
 	# Log current settings/preferences
-#	Log.Debug("### vlc_host= "+Prefs['vlc_host'])
-#	Log.Debug("### vlc_port= "+Prefs['vlc_port'])
-	url_vlc = 'http://%s:%s' % (Prefs['vlc_host'], Prefs['vlc_port']) # dynamic
-# Can't get the next line to work
-#	url_vlc = 'http://%s:%s/%s' % (Prefs['vlc_host'], Prefs['vlc_port'], Prefs['vlc_page']) # dynamic
+	Log.Debug("### vlc_host= "+Prefs['vlc_host'])
+	Log.Debug("### vlc_port= "+Prefs['vlc_port'])
+#	url_vlc = 'http://%s:%s' % (Prefs['vlc_host'], Prefs['vlc_port']) # dynamic
+	str_page = Prefs['vlc_page']
+	if str_page[0] != '/':
+		str_page = '/' + Prefs['vlc_page'] # does not start with a "/"
+	url_vlc = 'http://%s:%s%s' % (Prefs['vlc_host'], Prefs['vlc_port'], str_page) # dynamic
 	Log.Debug("### vlc_url= "+url_vlc)
 	
-# the following strategy does not appear to work (yet)
+# the following strategy does appear to work
 	mo = MediaObject(parts=[PartObject(key=HTTPLiveStreamURL(url_vlc))])
 	# the following instruction causes the framework to call the URL service
 	# see: \Contents\Info.plist -> PlexURLServices
@@ -102,7 +124,7 @@ def MainMenu():
 	vco = VideoClipObject(title="Play VLC Stream", url=url_vlc)
 	vco.add(mo)
 	
-# Not using the URL service is not working yet
+# the following strategy does not appear to work (yet)
 #	vco = CreateVideoClipObject(url_vlc, datetime.date.today())
 	oc.add(vco)
 	# provide for changing the host and port
@@ -173,6 +195,8 @@ def CreateVideoClipObject(url, originally_available_at, include_container=False)
 	except:
 		genres = ['Unknown genre']
 	
+#	types = demjson.encode(Container) # Framework.api.constkit.Containers
+#	Log.Debug("### "+Container.MP4)
 	if isinstance(originally_available_at, str):
 		Log.Debug("### STR OAA= "+originally_available_at)
 		originally_available_at = datetime.datetime.strptime(originally_available_at, '%Y-%m-%d')
@@ -185,6 +209,8 @@ def CreateVideoClipObject(url, originally_available_at, include_container=False)
 	vco = VideoClipObject(
 		key = Callback(CreateVideoClipObject, url=url, originally_available_at=originally_available_at, include_container=True),
 		rating_key = url,
+#		key = Callback(Callback(PlayVideo, url=url, default_fmt='360p'),
+#		rating_key = url,
 		title = title,
 		summary = summary,
 		thumb = Resource.ContentsOfURLWithFallback(thumb),
